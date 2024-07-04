e47d4fae29fa9781ccaedd8456e371e2e537e014
Sameer Sayyad <sameer07x19@gmail.com>
2024-07-04 20:36:22 +0530

    Update

--- app.js
+++ app.js
@@ -6,15 +6,30 @@ const port = 3000;
 
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(express.static("public"))
+
+
+
+
 app.get("/", (req,res) => {
     res.render("home.ejs")
 })
-app.post("/compose",(req,res) => {
-    res.render("compose.ejs")
+
+app.post("/newPost",(req,res) => {
+    res.render("newPost.ejs")
 })
+
 app.post("/myblogs",(req,res) => {
-    res.render("myBlogs.ejs")
+    var title = req.body.title;
+    var content = req.body.content;
+    res.render("myBlogs.ejs",{
+        title: title,
+        content: content
+    })
 })
+
+
+
+
 app.listen(port, () => {
     console.log(`Listening at port ${port}`)
 })
